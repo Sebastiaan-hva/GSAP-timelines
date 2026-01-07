47724a62828299ba7f2a57714a391da66f032f4f
SebastiaanHag <sebastiaan.hagoort@hva.nl>
2026-01-07 16:02:47 +0100

    scene 4 aangepast. positioning van rings moet nog anders

--- static/js/scene-four.js
+++ static/js/scene-four.js
@@ -2,15 +2,18 @@ import { DOM } from './elements.js'
 
 /* main timeline */
 export function createSceneFour() {
-  return gsap.timeline({ defaults: {duration: 1 }, paused: true })
+  return gsap.timeline({ defaults: { duration: 0.5 }, paused: true })
     .pause()
-    .add(tl(), .25)
+    .add(tl(), .1)
 }
 
 /* child timelines */
 function tl() {
   return gsap.timeline({})
-    .to(document.body, .5, { rotation: 10, x: 100, scale:.5}, '+=.8')
-    .to(document.body, .5, { rotation: 0, x: 0, scale:1, skewX:0, ease: 'elastic.out(1, 0.3)' }, '+=0.25')
+    .to(DOM.scene4Trigger, 0.15, { opacity: 0, yPercent: 100 })
+    .set([DOM.alienEmoji, DOM.rings], { position: 'fixed', left: '50vw', top: '50vh', x: 0, y: 0, xPercent: -50, yPercent: -50, transformOrigin: 'center', zIndex: 100, pointerEvents: 'none', width: 'auto', display: 'inline-block', textAlign: 'center', margin: 0, right: 'auto' })
+    .set(DOM.rings, { opacity: 0, scale: 0 })
+    .to(DOM.alienEmoji, 0.5, { scale: 1.8, rotation: 360, opacity: 1, ease: 'back.out(1.7)' })
+    .to(DOM.rings, 0.4, { opacity: 1, scale: 0.6, ease: 'power2.out' }, '<.1')
+    .to(DOM.rings, 1.5, { rotation: 360, repeat: 1, ease: 'linear' }, '<')
 }
-
